486eb6dedb97232b95d645092b501ec7b3c73bdd
cmadjar <cecile.madjar@gmail.com>
2025-03-07 16:30:48 -0500

    print

--- python/lib/import_dicom_study/summary_get.py
+++ python/lib/import_dicom_study/summary_get.py
@@ -38,6 +38,7 @@ def get_dicom_study_summary(dicom_study_dir_path: str, verbose: bool):
 
         try:
             dicom = pydicom.dcmread(file_path)  # type: ignore
+            print(dicom)
             if study_info is None:
                 study_info = get_dicom_study_info(dicom)
 
